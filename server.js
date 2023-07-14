883117f57dee51cbf85b7a319b4fdaf8909500dd
Vitali <vitaliy.prymak@cix.csi.cuny.edu>
2023-07-14 17:59:02 -0400

    end points for posts & comments

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const session = require("express-session");
 const port = 4000;
 const bcrypt = require("bcryptjs");
 const { Post, Comment, User } = require("./models");
@@ -7,8 +8,212 @@ require("dotenv").config();
 
 app.use(express.json());
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 3600000, // 1 hour
+    },
+  })
+);
+
+const authenticateUser = (req, res, next) => {
+  if (!req.session.userId) {
+    return res
+      .status(401)
+      .json({ message: "You must be logged in to view this page." });
+  }
+  next();
+};
+
 app.get("/", (req, res) => {
   res.send("Welcome to the Blog !!!!");
+});
+
+// Get all the posts
+app.get("/posts", async (req, res) => {
+  try {
+    const allPosts = await Post.findAll();
+
+    res.status(200).json(allPosts);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+// Create a new Post
+app.post("/posts", async (req, res) => {
+  try {
+    const newPost = await Post.create(req.body);
+
+    res.status(201).json(newPost);
+  } catch (err) {
+    if (err.name === "SequelizeValidationError") {
+      return res.status(422).json({ errors: err.errors.map((e) => e.message) });
+    }
+    console.error(err);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+// Get a specific Post
+app.get("/posts/:id", async (req, res) => {
+  const blogId = parseInt(req.params.id, 10);
+
+  try {
+    const Post = await Post.findOne({ where: { id: blogId } });
+
+    if (Post) {
+      res.status(200).json(Post);
+    } else {
+      res.status(404).send({ message: "Post not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+// Update a specific Post
+app.patch("/posts/:id", async (req, res) => {
+  const blogId = parseInt(req.params.id, 10);
+
+  try {
+    const [numberOfAffectedRows, affectedRows] = await Post.update(req.body, {
+      where: { id: blogId },
+      returning: true,
+    });
+
+    if (numberOfAffectedRows > 0) {
+      res.status(200).json(affectedRows[0]);
+    } else {
+      res.status(404).send({ message: "Post not found" });
+    }
+  } catch (err) {
+    if (err.name === "SequelizeValidationError") {
+      return res.status(422).json({ errors: err.errors.map((e) => e.message) });
+    }
+    console.error(err);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+// Delete a specific Post
+app.delete("/posts/:id", async (req, res) => {
+  const blogId = parseInt(req.params.id, 10);
+
+  try {
+    const deleteOp = await Post.destroy({ where: { id: blogId } });
+
+    if (deleteOp > 0) {
+      res.status(200).send({ message: "Post deleted successfully" });
+    } else {
+      res.status(404).send({ message: "Post not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+//////////////////////////// comments routes
+// Get all the posts
+app.post("/comments", async (req, res) => {
+  try {
+    const newComment = await Comment.create(req.body);
+
+    res.status(201).json(newComment);
+  } catch (err) {
+    if (err.name === "SequelizeValidationError") {
+      return res.status(422).json({ errors: err.errors.map((e) => e.message) });
+    }
+    console.error(err);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+// Get all comments
+app.get("/comments", async (req, res) => {
+  try {
+    const allcomments = await Comment.findAll();
+
+    res.status(200).json(allcomments);
+  } catch (err) {
+    if (err.name === "SequelizeValidationError") {
+      return res.status(422).json({ errors: err.errors.map((e) => e.message) });
+    }
+    console.error(err);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+// Get a specific comment
+app.get("/comments/:id", async (req, res) => {
+  const commentId = parseInt(req.params.id, 10);
+
+  try {
+    const comment = await Comment.findOne({ where: { id: commentId } });
+
+    if (comment) {
+      res.status(200).json(comment);
+    } else {
+      res.status(404).send({ message: "Comment not found" });
+    }
+  } catch (err) {
+    if (err.name === "SequelizeValidationError") {
+      return res.status(422).json({ errors: err.errors.map((e) => e.message) });
+    }
+    console.error(err);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+// Update a specific recipe
+app.patch("/comments/:id", async (req, res) => {
+  const commentId = parseInt(req.params.id, 10);
+
+  try {
+    const [numberOfAffectedRows, affectedRows] = await Comment.update(
+      req.body,
+      { where: { id: commentId }, returning: true }
+    );
+
+    if (numberOfAffectedRows > 0) {
+      res.status(200).json(affectedRows[0]);
+    } else {
+      res.status(404).send({ message: "Comment not found" });
+    }
+  } catch (err) {
+    if (err.name === "SequelizeValidationError") {
+      return res.status(422).json({ errors: err.errors.map((e) => e.message) });
+    }
+    res.status(500).send({ message: err.message });
+    console.error(err);
+  }
+});
+
+// Delete a specific recipe
+app.delete("/comments/:id", async (req, res) => {
+  const commentId = parseInt(req.params.id, 10);
+
+  try {
+    const deleteOp = await Comment.destroy({ where: { id: commentId } });
+
+    if (deleteOp > 0) {
+      res.status(200).send({ message: "Comment deleted successfully" });
+    } else {
+      res.status(404).send({ message: "Comment not found" });
+    }
+  } catch (err) {
+    if (err.name === "SequelizeValidationError") {
+      return res.status(422).json({ errors: err.errors.map((e) => e.message) });
+    }
+    console.error(err);
+    res.status(500).send({ message: err.message });
+  }
 });
 
 app.post("/signup", async (req, res) => {
@@ -20,7 +225,7 @@ app.post("/signup", async (req, res) => {
       email: req.body.email,
       password: hashedPassword,
     });
-
+    req.session.userId = user.id;
     // Send a response to the client informing them that the user was successfully created
     res.status(201).json({
       message: "User created!",
@@ -59,7 +264,7 @@ app.post("/login", async (req, res) => {
       if (result) {
         // Passwords match
         // TODO: Create a session for this user
-
+        req.session.userId = user.id;
         res.status(200).json({
           message: "Logged in successfully",
           user: {
@@ -77,6 +282,17 @@ app.post("/login", async (req, res) => {
       .status(500)
       .json({ message: "An error occurred during the login process" });
   }
+});
+
+app.delete("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.sendStatus(500);
+    }
+
+    res.clearCookie("connect.sid");
+    return res.sendStatus(200);
+  });
 });
 
 app.listen(port, () => {
